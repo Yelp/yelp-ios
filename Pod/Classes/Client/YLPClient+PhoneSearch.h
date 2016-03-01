f40adc2936c9d7f0a83f17ee2800bc3a2cf35412
David Chen <ywchen@yelp.com>
2016-02-29 17:23:05 -0800

    removed nullable completion handler in phone search client

--- Pod/Classes/Client/YLPClient+PhoneSearch.h
+++ Pod/Classes/Client/YLPClient+PhoneSearch.h
@@ -13,7 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface YLPClient (PhoneSearch)
 
-- (void)getBusinessWithPhoneNumber:(NSString *)phoneNumber countryCode:(nullable NSString *)countryCode category:(nullable NSString *)category completionHandler:(nullable void (^)(YLPPhoneSearch * _Nullable phoneSearch, NSError * _Nullable error))completionHandler;
+- (void)getBusinessWithPhoneNumber:(NSString *)phoneNumber countryCode:(nullable NSString *)countryCode category:(nullable NSString *)category completionHandler:(void (^)(YLPPhoneSearch * _Nullable phoneSearch, NSError * _Nullable error))completionHandler;
 
 - (void)getBusinessWithPhoneNumber:(NSString *)phoneNumber completionHandler:(void (^)(YLPPhoneSearch * _Nullable phoneSearch, NSError * _Nullable error))completionHandler;
 
