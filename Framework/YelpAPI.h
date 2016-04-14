3f5c17934e27f0de128ce760ab97b8c70cc65852
Steven Sheldon <steven@sasheldon.com>
2016-04-13 18:19:31 -0700

    Create a YelpAPI framework in the xcodeproj.

---
+++ Framework/YelpAPI.h
@@ -0,0 +1,19 @@
+//
+//  YelpAPI.h
+//  YelpAPI
+//
+//  Created by Steven Sheldon on 4/13/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YelpAPI.
+FOUNDATION_EXPORT double YelpAPIVersionNumber;
+
+//! Project version string for YelpAPI.
+FOUNDATION_EXPORT const unsigned char YelpAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YelpAPI/PublicHeader.h>
+
+
