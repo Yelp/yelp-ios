3b7aa9e2b12ccb4c311bbdcd102ddbb74fb7e41d
Steven Sheldon <steven@sasheldon.com>
2016-05-14 10:09:50 -0700

    Move umbrella header to be included in the pod.

---
+++ Classes/YelpAPI.h
@@ -0,0 +1,32 @@
+//
+//  YelpAPI.h
+//  YelpAPI
+//
+//  Created by Steven Sheldon on 4/13/16.
+//
+//
+
+#import "YLPClient.h"
+#import "YLPClient+Business.h"
+#import "YLPClient+PhoneSearch.h"
+#import "YLPClient+Search.h"
+
+#import "YLPCoordinate.h"
+
+#import "YLPGeoBoundingBox.h"
+#import "YLPGeoCoordinate.h"
+#import "YLPSortType.h"
+
+#import "YLPBusiness.h"
+#import "YLPCategory.h"
+#import "YLPCoordinateDelta.h"
+#import "YLPDeal.h"
+#import "YLPDealOption.h"
+#import "YLPGiftCertificate.h"
+#import "YLPGiftCertificateOption.h"
+#import "YLPLocation.h"
+#import "YLPPhoneSearch.h"
+#import "YLPRegion.h"
+#import "YLPReview.h"
+#import "YLPSearch.h"
+#import "YLPUser.h"
